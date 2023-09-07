c3a1987a2c202bb7a98131249775aff2502e7a90
Puja <pujak816@gmail.com>
2023-09-07 16:23:18 +0530

    .env file added

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -5,7 +5,7 @@ import RestoreOutlinedIcon from "@mui/icons-material/RestoreOutlined";
 import WatchLaterOutlinedIcon from "@mui/icons-material/WatchLaterOutlined";
 import SlideshowOutlinedIcon from "@mui/icons-material/SlideshowOutlined";
 
-const GOOGLE_API_KEY = "AIzaSyBnqsmZe_k3S-0MSEi_yJCPtQZs5zU8Ng8";
+const GOOGLE_API_KEY = process.env.REACT_APP_API_KEY;
 
 export const YOUTUBE_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
