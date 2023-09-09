3710e127a5d114a1678f4eaccd5156afe0f634de
Puja <pujak816@gmail.com>
2023-09-09 10:01:25 +0530

    fix: CORS Error

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -11,8 +11,12 @@ export const YOUTUBE_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+const searchUrl =
+  "https://corsproxy.io/?" +
+  encodeURIComponent(
+    "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+  );
+export const YOUTUBE_SEARCH_API = searchUrl;
 
 export const YOUTUBE_COMMENTS_API =
   "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&allThreadsRelatedToChannelId=UCfLuT3JwLx8rvHjHfTymekw&key=" +
