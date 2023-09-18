6e1c77a68701a99d47bc10c6319f1280bf1714bc
Puja <pujak816@gmail.com>
2023-09-18 10:06:24 +0530

    refactor - error handling

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 
 function VideoContainer() {
   const [videos, setVideos] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     getVideos();
@@ -12,19 +13,29 @@ function VideoContainer() {
 
   const getVideos = async () => {
     const data = await fetch(YOUTUBE_API);
+    if (!data.ok) {
+      setError(true);
+      return;
+    }
     const json = await data.json();
     setVideos(json.items);
   };
 
   return (
     <>
-      <div className="flex flex-wrap pt-10 justify-center">
-        {videos.map((item) => (
-          <Link key={item.id} to={"/watch?v=" + item.id}>
-            <VideoCard videoInfo={item} />
-          </Link>
-        ))}
-      </div>
+      {!error ? (
+        <div className="flex flex-wrap pt-10 justify-center">
+          {videos.map((item) => (
+            <Link key={item.id} to={"/watch?v=" + item.id}>
+              <VideoCard videoInfo={item} />
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <p className="pt-32 px-20 text-2xl  aspect-square w-full  fixed  ">
+          Error getting videos API Limit exceeded
+        </p>
+      )}
     </>
   );
 }
